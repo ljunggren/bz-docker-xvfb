9a5e1547be7dea695687bc7388e060e2921c0df7
ljunggren <mljunggren@gmail.com>
2020-05-14 19:59:47 -0400

    Sync. with bz-puppeteer

--- process.sh
+++ process.sh
@@ -5,8 +5,8 @@ rm -rf *.png
 rm -rf *.list
 
 # Copy latest from master
-cp ~/Workspace/bz-puppeteer/bz-extension/* bz-extension/.
 cp -r ~/Workspace/bz-puppeteer/bz-extension/* bz-extension/.
+cp -r ~/Workspace/bz-puppeteer/index.js index.js
 
 # Docker build and push
 docker build -t styrman/boozang-runner .
