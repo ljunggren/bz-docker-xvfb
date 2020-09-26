73ed4322068cccca1bcbb5ac016c2b1ffb2aad24
ljunggren <mljunggren@gmail.com>
2020-09-26 17:09:56 -0400

    Sync. with bz-puppeteer

--- wrap.sh
+++ wrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir /root/Downloads/
 Xvfb :99 -screen 0 1280x1024x16 & node index --docker $@
-rsync -avzh --ignore-errors /root/Downloads/* /var/boozang/
+cp /root/Downloads/* /var/boozang/
